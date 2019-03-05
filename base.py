daf93c20560ad36a2a9e9fad5771864e19a409e0
Noah Graells <noah.graells@he-arc.ch>
2019-03-05 15:03:06 +0100

    Tesseract tests and code cleaning

--- base.py
+++ base.py
@@ -20,7 +20,8 @@ except:
     exit()
 
 # ------------------- load the image -------------------
-img = cv2.imread('rotated_basic_addition.jpg',0)
+# img = cv2.imread('rotated_basic_addition.jpg',0)
+img = cv2.imread('clean_all.jpg',0)
 cv2.imshow('image', img)
 cv2.waitKey(0)
 
@@ -43,20 +44,22 @@ for cnt in contours[:-1]:
 contours_in_one = np.array(contours_in_one)
 
 # ------------------- find the bounding rect of the image and rotate -------------------
-box = cv2.minAreaRect(contours_in_one) # returns a box2D object ( center (x,y), (width, height), angle of rotation ).
-w = box[1][0]
-h = box[1][1]
-x = box[0][0] - h/2
-y = box[0][1] - w/2
-angle = box[2]
-w, h, x, y = int(w), int(h), int(x), int(y)
+# box = cv2.minAreaRect(contours_in_one) # returns a box2D object ( center (x,y), (width, height), angle of rotation ).
+# w = box[1][0]
+# h = box[1][1]
+# x = box[0][0] - h/2
+# y = box[0][1] - w/2
+# angle = box[2]
+# w, h, x, y = int(w), int(h), int(x), int(y)
 
-print(-angle)
-rows,cols = img.shape
-M = cv2.getRotationMatrix2D((cols/2,rows/2),-angle,1) # angle + 90
-img = cv2.warpAffine(img,M,(cols,rows))
-cv2.imshow('image test', img)
-cv2.waitKey(0)
+# print(-angle)
+# rows,cols = img.shape
+# M = cv2.getRotationMatrix2D((cols/2,rows/2),angle+90,1) # angle + 90
+# img = cv2.warpAffine(img,M,(cols,rows))
+# cv2.imshow('image test', img)
+# cv2.waitKey(0)
+
+img_test = img.copy()
 
 # no comment _-----_
 # img_bounding_rect2 = img
@@ -85,11 +88,11 @@ cv2.waitKey(0)
 im, contours, hierarchy = cv2.findContours(img_thresh, 1, 2)
 image_individual_bounding_rect = img
 elements = []
-for cnt in contours[:]:
+for cnt in contours[:-1]:
         x,y,w,h = cv2.boundingRect(cnt)
         bounding_rect = [[x, y], [x+w, y], [x+w, y+h], [x, y+h]]
         cv2.rectangle(image_individual_bounding_rect,tuple(bounding_rect[0]),tuple(bounding_rect[2]),(0,255,0),2)
-        elements.append(img[y:y+h, x:x+w])
+        elements.append(img_test[y:y+h, x:x+w])
 
 cv2.imshow('image individual bounding rect', image_individual_bounding_rect)
 cv2.waitKey(0)
@@ -101,19 +104,13 @@ for elem in elements:
     i += 1
 
 # ------------------- tesseract part -------------------
-# i = 0
-# for elem in elements:
-#     filename = "element%d.png" % i
-#     cv2.imwrite(filename, elements[i])
-#     text = pytesseract.image_to_string(Image.open(filename))
-#     cv2.imshow('elem%d : %s' % (i, text), elem)
-#     cv2.waitKey(0)
-#     i += 1
-#
-# os.remove(filename)
-# print(text)
 
+for elem in elements:
 
+    text = pytesseract.image_to_string(elem)
+    cv2.imshow('elem : %s' % (text), elem)
+    cv2.waitKey(0)
+    print(text)
 
 
 cv2.destroyAllWindows()
